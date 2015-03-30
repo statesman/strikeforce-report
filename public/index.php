e7133da05f80e7d1452510bb821a8433cf82a77b
Andrew Chavez <adchavez@gmail.com>
2015-03-30 18:00:53 -0500

    Add social description

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
     <?php
       $meta = array(
         'title' => "8 key passages from the 21CT ‘strike force’ report",
-        'description' => '',
+        'description' => 'Read key excerpts from the 92-page report released by the “strike force” assembled in the wake of the 21CT contracting scandal.',
         'thumbnail' => 'http://projects.statesman.com/news/strikeforce-report/assets/share.jpg',
         'url' => 'http://projects.statesman.com/news/strikeforce-report/',
         'twitter' => 'statesman'
